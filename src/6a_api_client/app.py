9acf87cbae81956d1b7ae022cf772d4e9b2d1eda
smurilloo <143961450+smurilloo@users.noreply.github.com>
2024-04-06 11:20:53 -0500

    Sol_nuevo
    Nuevo

--- src/6a_api_client/app.py
+++ src/6a_api_client/app.py
@@ -85,4 +85,5 @@ if __name__ == "__main__":
     app.run(debug=True, port=5002)
     logging.info("Starting the application")
 
+#Correr ubicado en la ruta del proyecto, no en src
 #python src/6a_api_client/app.py
